b79aa56aee6f3d53b34dce4229e8cbdedfaeb3fe
leopheard <leopheard.misc@gmail.com>
2019-08-05 21:31:37 -0400

    upload

---
+++ README.md
@@ -0,0 +1,9 @@
+Morbid: A True Crime Podcast<br>
+=============================
+
+<a href="www.kodi.tv">KODI</a> / XBMC addon for the audio <a href="https://www.morbidpodcast.com/">Morbid: A True Crime Podcast</a>.
+
+<a href="https://www.morbidpodcast.com/">Morbid: A True Crime Podcast</a> was born out of a genuine love for true crime and all things spooky. We cover serial killers, unsolved mysteries, spooky history, paranormal happenings, conspiracy theories and anything that sends chills down your spine and gets your mind working overtime.<br>
+
+^^^ To install this addon, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
+<a href="https://www.morbidpodcast.com"><img src="https://images.squarespace-cdn.com/content/5c2e8a84372b96d2491fec2c/1546556806188-TX2D7ASHP5MB3XKNGEV5/20180901_160534.jpg?content-type=image%2Fjpeg">
---
+++ addon.py
@@ -0,0 +1,48 @@
+from xbmcswift2 import Plugin, xbmcgui
+from resources.lib import mainaddon
+
+plugin = Plugin()
+
+# base url for fetching podcasts 
+URL = "https://audioboom.com/channels/4997220.rss"
+
+@plugin.route('/')
+def main_menu():
+    """
+    main menu 
+    """
+    items = [
+        {
+            'label': plugin.get_string(30000), 
+            'path': plugin.url_for('all_episodes'),
+            'thumbnail': "https://images.theabcdn.com/i/34958472.jpg"},
+   {
+            'label': plugin.get_string(30001), 
+            'path': plugin.url_for('all_episodes1'),
+            'thumbnail': "https://images.theabcdn.com/i/34958472.jpg"},
+   ]
+    return items
+
+@plugin.route('/all_episodes/')
+def all_episodes():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = mainaddon.get_soup(URL)
+    playable_podcast = mainaddon.get_playable_podcast(soup)
+    items = mainaddon.compile_playable_podcast(playable_podcast)
+    return items
+
+
+@plugin.route('/all_episodes1/')
+def all_episodes1():
+    """
+    contains playable podcasts listed as just-in
+    """
+    soup = mainaddon.get_soup(URL)
+    playable_podcast1 = mainaddon.get_playable_podcast1(soup)
+    items = mainaddon.compile_playable_podcast1(playable_podcast1)
+    return items
+
+if __name__ == '__main__':
+    plugin.run()
